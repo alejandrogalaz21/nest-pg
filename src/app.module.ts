7b196808ddc633bd4b23c95c73e056587cf04079
Jesus Alejandro Galaz Icedo <jesus.galaz@softtek.com>
2024-03-21 12:47:44 -0700

    add pg type orm connection and env docker-compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.ROOT_USER,
+      password: process.env.DB_PASSWORD,
+      database: 'pg_db_test',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
