c44ce032ecabdd18deb12daf763279ecf09e100d
Jesus Alejandro Galaz Icedo <jesus.galaz@softtek.com>
2024-03-21 17:56:56 -0700

    add global config to validate payload

--- src/cars/dto/create-car.dto.ts
+++ src/cars/dto/create-car.dto.ts
@@ -1,9 +1,11 @@
-import { IsString } from 'class-validator'
+import { IsString, MinLength } from 'class-validator'
 
 export class CreateCarDto {
   @IsString()
+  @MinLength(3)
   readonly brand: string
 
   @IsString()
+  @MinLength(3)
   readonly model: string
 }
